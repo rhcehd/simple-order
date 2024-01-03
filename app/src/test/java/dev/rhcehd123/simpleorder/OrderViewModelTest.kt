f7cc6190105a8c3f6b52e862280684f673cc49bb
rhcehd <lhs9403@naver.com>
2024-01-03 17:53:59 +0900

    Change Kotest config for Android CI

--- app/src/test/java/dev/rhcehd123/simpleorder/OrderViewModelTest.kt
+++ app/src/test/java/dev/rhcehd123/simpleorder/OrderViewModelTest.kt
@@ -5,6 +5,7 @@ import dev.rhcehd123.simpleorder.data.testOptions
 import dev.rhcehd123.simpleorder.data.testOrderItems
 import dev.rhcehd123.simpleorder.ui.order.OrderUiState
 import dev.rhcehd123.simpleorder.ui.order.OrderViewModel
+import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
@@ -16,27 +17,34 @@ import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class OrderViewModelTest: BehaviorSpec() {
-    init {
-        Dispatchers.setMain(StandardTestDispatcher())
-        coroutineTestScope = true
-        val orderRepository = mockk<OrderRepository>()
-        coEvery { orderRepository.getOrderItems().getOrNull() } returns testOrderItems
-        coEvery { orderRepository.getAvailableOptions().getOrNull() } returns testOptions
-        val viewModel = OrderViewModel(orderRepository)
+class OrderViewModelTest: BehaviorSpec({
+    val orderRepository = mockk<OrderRepository>()
+    coEvery { orderRepository.getOrderItems().getOrNull() } returns testOrderItems
+    coEvery { orderRepository.getAvailableOptions().getOrNull() } returns testOptions
+    val viewModel = OrderViewModel(orderRepository)
 
-        Given("주문 리스트 진입 후") {
-            When("주문 아이템을 선택 하지 않았을 때") {
-                Then("주문 리스트의 수가 기대 값과 일치 한다") {
-                    val currentOrderUiState = viewModel.uiState.value as OrderUiState.NoSelectedItem
-                    currentOrderUiState.orderItemList.size shouldBe testOrderItems.size
-                }
-            }
-            When("주문 아이템을 선택 했을 때") {
-                Then("hello") {
-                    0 shouldBe 0
-                }
+    Given("주문 리스트 진입 후") {
+        When("주문 아이템을 선택 하지 않았을 때") {
+            Then("주문 리스트의 수가 기대 값과 일치 한다") {
+                val currentOrderUiState = viewModel.uiState.value as OrderUiState.NoSelectedItem
+                currentOrderUiState.orderItemList.size shouldBe testOrderItems.size
             }
         }
+        When("주문 아이템을 선택 했을 때") {
+            Then("hello") {
+                0 shouldBe 0
+            }
+        }
+    }
+}) {
+    override suspend fun beforeSpec(spec: Spec) {
+        super.beforeSpec(spec)
+        Dispatchers.setMain(StandardTestDispatcher())
+        coroutineTestScope = true
+    }
+
+    override suspend fun afterSpec(spec: Spec) {
+        super.afterSpec(spec)
+        Dispatchers.resetMain()
     }
 }
--- app/src/test/java/dev/rhcehd123/simpleorder/config/ProjectConfig.kt
+++ app/src/test/java/dev/rhcehd123/simpleorder/config/ProjectConfig.kt
@@ -4,20 +4,22 @@ import io.kotest.core.config.AbstractProjectConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 
+/*
 @OptIn(ExperimentalCoroutinesApi::class)
 object ProjectConfig: AbstractProjectConfig() {
-
+    private val testDispatcher = StandardTestDispatcher()
+    //override val coroutineDebugProbes: Boolean = true
+    //override val coroutineTestScope: Boolean = true
     override suspend fun beforeProject() {
         super.beforeProject()
-        Dispatchers.setMain(StandardTestDispatcher())
+        Dispatchers.setMain(testDispatcher)
     }
 
     override suspend fun afterProject() {
         super.afterProject()
         Dispatchers.resetMain()
     }
-}
+}*/
